4fbdca2fd6f67e489b413fbac171e061758ab7c3
Abdul Vahid <shaikabdul.wahid@gmail.com>
2022-11-05 05:17:45 +0000

    New Job Modified

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Good Morning from Abdul to all!!!");
+	  System.out.println("Hello from Main Method & Good Morning to all!!!");
   }
 	
 }
