44d03bdb9c15d59daa24b25cc2aed8fa23e616be
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2023-03-16 12:21:55 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello and Good Afternoon !!!");
+	  System.out.println("Hello from Main Method !!!");
   }
 	
 }
